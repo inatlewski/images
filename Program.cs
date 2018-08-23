3a7578916b0ad9b79d105ee62a0e56937f69529c
Unknown <lukasz.inatlewski@gmail.com>
2018-08-23 21:35:46 +0200

    Renaming of the project

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace images
+namespace Images
 {
     public class Program
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace images
+namespace Images
 {
     public class Startup
     {
