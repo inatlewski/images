365fd953ee8218b5878291b86511ec382c17213a
Lukasz Inatlewski <lukasz@inatlewski.net>
2018-08-27 23:19:03 +0200

    Set up typical solution structure.

---
+++ Images.BusinessLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Images.BusinessLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ Images.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Images.Common
+{
+    public class Class1
+    {
+    }
+}
