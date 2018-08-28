0c49d19992eeb335e9be23fba04b9edc4237e525
Unknown <lukasz.inatlewski@gmail.com>
2018-08-29 00:03:24 +0200

    Added Entities and DTOs.

---
+++ Images.Model/DTO/AuditDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Images.Model.DTO
+{
+    public class AuditDto
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Images.Model/Entities/Image.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Images.Model.Entities
+{
+    public class Image : Auditable
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
